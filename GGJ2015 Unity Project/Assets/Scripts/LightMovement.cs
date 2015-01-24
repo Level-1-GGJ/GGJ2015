d2c99fc52f0b97a098d7dd80deec1ae6c37271b4
unknown <wsw3877@GDD-26.main.ad.rit.edu>
2015-01-24 13:27:24 -0500

    Made the light follow the character up until a certain point. Yay.

---
+++ GGJ2015 Unity Project/Assets/Scripts/LightMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MovementController))]
+public class LightMovement : MonoBehaviour 
+{
+    MovementController playerController;
+    MovementController mc;
+
+    public Vector2 rightBound;
+    public Vector2 leftBound;
+
+	// Use this for initialization
+	void Start () 
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<MovementController>();
+        mc = GetComponent<MovementController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (playerController.Position.x < rightBound.x && playerController.Position.x > leftBound.x)
+        {
+            mc.Position = new Vector2(playerController.Position.x,mc.Position.y);
+        }
+	}
+}
