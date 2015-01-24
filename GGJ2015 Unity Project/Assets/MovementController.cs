e1a6e8a8e5b9f4667125416fb8ab8ce3c6fc6d49
unknown <sis2773@GDD-27.main.ad.rit.edu>
2015-01-23 20:21:53 -0500

    Basic MovementController added

---
+++ GGJ2015 Unity Project/Assets/MovementController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = rigidbody2D;
+        rb.fixedAngle = true;        
+	}
+
+    void Move(Vector2 direction)
+    {
+        rb.MovePosition(rb.position + direction);
+    }
+
+    void AddForce(Vector2 force)
+    {
+        rb.AddForce(force);
+    }
+}
