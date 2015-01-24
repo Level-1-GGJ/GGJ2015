5303de1a26b397b1fda3988ed91cfe6d04d6b8dd
unknown <wsw3877@GDD-26.main.ad.rit.edu>
2015-01-24 11:55:21 -0500

    Added the light changing script. It changes lights and at a certain rate for each light. Also added textures for the lights. They are temporary for now

---
+++ GGJ2015 Unity Project/Assets/Scripts/LightSwitcher.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MovementController))]
+public class LightSwitcher : MonoBehaviour 
+{
+    public Texture[] lights;
+    public float greenTime;
+    public float yellowTime;
+    public float redTime;
+
+    float timePassed = 0;
+    int curLight = 0;
+
+    public bool isGreen;
+    public bool isYellow;
+    public bool isRed;
+
+    MovementController mc;
+
+	// Use this for initialization
+	void Start () 
+    {
+        mc = GetComponent<MovementController>();
+        isGreen = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (isGreen)
+        {
+            if (timePassed > greenTime)
+            {
+                timePassed = 0;
+                curLight++;
+            }
+        }
+        else if (isYellow)
+        {
+            if (timePassed > yellowTime)
+            {
+                timePassed = 0;
+                curLight++;
+            }
+        }
+        else if (isRed)
+        {
+            if (timePassed > redTime)
+            {
+                timePassed = 0;
+                curLight++;
+            }
+        }
+        timePassed += Time.deltaTime;
+        renderer.material.mainTexture = lights[curLight % 3];
+	}
+}
