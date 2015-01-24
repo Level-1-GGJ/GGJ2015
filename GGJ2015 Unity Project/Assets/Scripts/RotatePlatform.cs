f016876d9903006d59d6a350835c410c4431a51e
LunaLovecraft <lunalovecraft@gmail.com>
2015-01-24 17:10:47 -0500

    Added rotating platforms

---
+++ GGJ2015 Unity Project/Assets/Scripts/RotatePlatform.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePlatform : MonoBehaviour
+{
+    public float startAngle = 0;
+    public float endAngle = 360;
+    public float rotationSpeed;
+    public bool isLooping = true;
+    public bool goForward = true;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (goForward)
+        {
+            this.rigidbody2D.rotation = this.rigidbody2D.rotation + rotationSpeed;
+
+            if (this.rigidbody2D.rotation > endAngle)
+            {
+                if (isLooping)
+                {
+                    this.rigidbody2D.rotation = startAngle;
+                }
+                else
+                {
+                    goForward = false;
+                }
+            }
+        }
+        else
+        {
+            this.rigidbody2D.rotation = this.rigidbody2D.rotation - rotationSpeed;
+
+            if (this.rigidbody2D.rotation <= startAngle)
+            {
+                if (isLooping)
+                {
+                    this.rigidbody2D.rotation = endAngle;
+                }
+                else
+                {
+                    goForward = true;
+                }
+            }
+        }
+    }
+}
