27f3636ec3afffb43f7ffecaae39b8f5adfd209a
unknown <wsw3877@GDD-26.main.ad.rit.edu>
2015-01-25 12:40:17 -0500

    Made the main menu scene and some scripts for it

---
+++ GGJ2015 Unity Project/Assets/Scripts/TitleScreenScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenScript : MonoBehaviour {
+
+    public string nextScene;
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    public void load()
+    {
+        Application.LoadLevel(nextScene);
+    }
+}
