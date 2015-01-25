1995bf04b973b0e87d0bedc5f4e9516db04c948b
unknown <wsw3877@GDD-26.main.ad.rit.edu>
2015-01-24 21:41:14 -0500

    added the streetlight model and made it light up with lights. Also did some other things that i dont remember

---
+++ GGJ2015 Unity Project/Assets/Scripts/Light/LightChanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightChanger : MonoBehaviour 
+{
+    LightSwitcher streetLight;
+    public int littleLight;
+    
+	// Use this for initialization
+	void Start () 
+    {
+        streetLight = GameObject.FindGameObjectWithTag("StreetLight").GetComponent<LightSwitcher>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (streetLight.curLight%3 == littleLight)
+        {
+            light.enabled = true;
+        }
+        else
+        {
+            light.enabled = false;
+        }
+	}
+}
